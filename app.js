e4fffe7c5bcf5a7f98554f387493aebc5e6ce0d6
Perinpam Senthuran <senthu200408@gmail.com>
2026-09-25 12:03:15 +0530

    DOM Manupulation

--- app.js
+++ app.js
@@ -1,32 +1,9 @@
-//================ usual and easiest object creation ================
+//================ DOM Manupulation ================
 
-let student = [
-    {
-        name: "John Doe",
-        age: 30,
-        address: "123 Main St",
-        marks: [
-            {
-                subject: "Science",
-                score: 85
-            },
-            {
-                subject: "Mathematics",
-                score: 80
-            }
-        ]
-    },
-    {
-        name: "Jane Smith",
-        age:25,
-        address: "456 Oak Ave"
-    },
-    {
-        name:"Alice Johnson",
-        age: 28,
-        address: "789 Elm St"
-    }
-]
+//console.log(student[0].marks[1].score);  //prints 80
+//console.log(student[0].marks[0].subject); //prints "Science"
 
-console.log(student[0].marks[1].score);  //prints 80
-console.log(student[0].marks[0].subject); //prints "Science"
+console.log(document); //prints the codes in the html document
+console.log(document.title);  //prints the title of the document
+document.write("<h1>Hello World!!!</h1>");  //prints the headint "hello world!!!"
+
